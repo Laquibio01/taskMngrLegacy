a1033367bf76dc6b9419c77874cd7b557a2f28fb
Laquibio01 <hernandeztrejo1234@gmail.com>
2026-01-30 00:15:41 -0600

    [Hooks] Create useKonamiCode hook structure

---
+++ frontend/src/hooks/useKonamiCode.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+
+const KONAMI_CODE = [
+    'ArrowUp',
+    'ArrowUp',
+    'ArrowDown',
+    'ArrowDown',
+    'ArrowLeft',
+    'ArrowRight',
+    'ArrowLeft',
+    'ArrowRight',
+    'b',
+    'a',
+];
+
+const useKonamiCode = (action) => {
+    const [input, setInput] = useState([]);
+
+    return null;
+};
+
+export default useKonamiCode;
